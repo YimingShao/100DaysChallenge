b13029850537503d5c81ea651c744f88c5b31f8a
Yiming Shao <shaoyiminglobo@gmailcom>
2018-05-28 14:04:04 -0400

    update

--- day46-48/day3/api.py
+++ day46-48/day3/api.py
@@ -4,7 +4,6 @@ import bs4
 
 
 URL = "my_file.html"
-sender = 'shaoyiminglobo@gmail.com'
 
 def scrape():
     header_list = []
@@ -17,11 +16,14 @@ def scrape():
 
     key = rec_input()
     if key is 'y':
-        target=input('Input your email:\t')
+        print('Note: I promise, i wont collect your data')
+        sender=input('Input your email:\t')
+        password = input('Input your password:\t')
+        target = input('Input who are you going to send:\t')
         try:
             server = smtplib.SMTP('smtp.gmail.com',587)
             server.starttls()
-            server.login(sender,'shao1234')
+            server.login(sender,password)
             msg = convert_to_text(header_list)
             server.sendmail(sender, target, msg)
             server.quit()
